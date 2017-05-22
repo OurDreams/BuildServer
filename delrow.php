111c776022e4ae33c2589b32f31130748fa43036
kayfelicities <390528655@qq.com>
2017-05-22 08:23:46 +0800

    del pw

--- delrow.php
+++ delrow.php
@@ -25,16 +25,51 @@
     mysqli_set_charset($link, "utf8");
     mysqli_select_db($link, 'buildserver');
     mysqli_query($link, 'set names utf8_bin');
-    mysqli_query($link, "UPDATE build_information SET status='del' WHERE build_id='$build_id'");
-    if (mysqli_affected_rows($link))
+    $result = mysqli_query($link, "SELECT status FROM build_information WHERE build_id='$build_id'");
+    $row = mysqli_fetch_array($result);
+    $is_pass = FALSE;
+    if ($row)
     {
-        echo ("<script>$.notify({message: '删除成功'}, {type: 'success'});</script>");
-        header("Refresh: 1; url=index.php");
+        if ($row['status'] == 'error')
+        {
+            if ($password == '000000' or $password == '220184')
+            {
+                $is_pass = TRUE;
+            }
+            else
+            {
+                echo ("<script>$.notify({message: '密码错误！'}, {type: 'danger'});</script>");
+            }
+        }
+        if ($row['status'] != 'error')
+        {
+            if ($password == '220184')
+            {
+                $is_pass = TRUE;
+            }
+            else
+            {
+                echo ("<script>$.notify({message: '正在编译或编译成功的项目无法删除！'}, {type: 'danger'});</script>");
+            }
+        }
+    }
+    if ($is_pass)
+    {
+        mysqli_query($link, "UPDATE build_information SET status='del' WHERE build_id='$build_id'");
+        if (mysqli_affected_rows($link))
+        {
+            echo ("<script>$.notify({message: '删除成功'}, {type: 'success'});</script>");
+            header("Refresh: 1; url=index.php");
+        }
+        else
+        {
+            echo ("<script>$.notify({message: '删除失败'}, {type: 'danger'});</script>");
+            header("Refresh: 3; url=index.php");
+        }
     }
     else
     {
-        echo ("<script>$.notify({message: '删除失败'}, {type: 'danger'});</script>");
-        header("Refresh: 1; url=index.php");
+        header("Refresh: 3; url=index.php");
     }
 ?>
 </div>
