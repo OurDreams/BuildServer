bf58c9b805676b82bacaede252060c6475021cc5
kayfelicities <390528655@qq.com>
2017-06-07 14:19:23 +0800

    space in svn url bug fix

--- svnlog.php
+++ svnlog.php
@@ -2,10 +2,12 @@
 
 <?php
 $svn_url=isset($_GET['url']) ? $_GET['url'] : '';
-$log = shell_exec('svn log -l 5 '.$svn_url);
+$log = shell_exec('svn log -l 5 "'.$svn_url.'"');
 $log = mb_convert_encoding($log, "UTF-8", "GBK");
+// print_r($log);
 
 preg_match_all("|\nr(.*?)\s\|\s(.*?)\s\|\s(.*?)\s\+.*?\n\n(.*?)\n|", $log, $out, PREG_PATTERN_ORDER);
+
 for ($i = 0; $i < count($out[0]); $i++)
 {
     // print_r($out[1][$i] . '<br>' . $out[2][$i] . '<br><br>');
